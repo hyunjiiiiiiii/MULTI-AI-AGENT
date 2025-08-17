fc68c4b81e2d07e5a0488fb2e1438627653f23b9
hyunjiiiiiiii <guswl1613@gmail.com>
2025-08-17 20:07:03 +0900

    0817 code review

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def run_backend():
     try:
         logger.info("starting backend service..")
         subprocess.run(["uvicorn", "app.backend.api:app", "--host", "127.0.0.1", "--port", "9999"], check=True)
-    except CustomException as e:
+    except Exception as e:
         logger.error("Problem with backend service")
         raise CustomException("Failed to start backend", e)
     
@@ -22,7 +22,7 @@ def run_frontend():
     try:
         logger.info("Starting Frontend service..")
         subprocess.run(["streamlit", "run", "app/frontend/ui.py"], check=True)
-    except CustomException as e:
+    except Exception as e:
         logger.error("Problem with frontend service")
         raise CustomException("Failed to start frontend", e)
         
@@ -31,5 +31,5 @@ if __name__=="__main__":
         threading.Thread(target=run_backend).start()
         time.sleep(2)
         run_frontend()
-    except CustomException as e:
+    except Exception as e:
         logger.exception(f"CustomException occured : {str(e)}")
